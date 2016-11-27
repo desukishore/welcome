225442ac194bb91745a2ae6afd03d6bf6fff07e8
Kishore Kumar Desu <desukishore>
2016-11-27 14:22:27 +0530

    spring 4 welcome project
    spring 4 welcome project

---
+++ src/main/java/net/kishore/spring/controller/HelloController.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package net.kishore.spring.controller;
+
+/**
+ * @author Krishna
+ *
+ */
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String hello(Model model) {
+
+		model.addAttribute("name", "Kishore Kumar Desu");
+
+		return "welcome";
+	}
+}
